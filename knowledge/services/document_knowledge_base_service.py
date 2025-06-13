222cbeb6f64cf6bca12699cc1384e90331991d75
Gustavo Ghioldi <gustavo.ghioldi@itti.digital>
2025-06-13 16:28:12 -0300

    add web scraping knoledge

--- knowledge/services/document_knowledge_base_service.py
+++ knowledge/services/document_knowledge_base_service.py
@@ -1,6 +1,8 @@
 from agno.document.base import Document
 from agno.embedder.ollama import OllamaEmbedder
+from agno.knowledge.combined import CombinedKnowledgeBase
 from agno.knowledge.document import DocumentKnowledgeBase
+from agno.knowledge.website import WebsiteKnowledgeBase
 from agno.vectordb.pgvector import PgVector
 
 from agents.models import AgentModel
@@ -14,10 +16,15 @@ class DocumentKnowledgeBaseService:
 
     def get_knowledge_base(self):
         documents = []
-        for k in self.agent_model.knoledge_text_models.all():
-            documents.append(Document(content=k.text))
+        sites = []
 
-        knowledge_base = DocumentKnowledgeBase(
+        for k in self.agent_model.knoledge_text_models.all():
+            if k.category == "plain_text":
+                documents.append(Document(content=k.text))
+            if k.category == "website":
+                sites.append(k.url)
+
+        knowledge_base_documents = DocumentKnowledgeBase(
             documents=documents,
             vector_db=PgVector(
                 table_name="ia_documents",
@@ -25,5 +32,29 @@ class DocumentKnowledgeBaseService:
                 embedder=OllamaEmbedder(id=IA_MODEL, dimensions=3072),
             ),
         )
-        knowledge_base.load(recreate=False)
-        return knowledge_base
+
+        knowledge_base_web = WebsiteKnowledgeBase(
+            urls=sites,
+            max_depth=1,
+            max_links=1,
+            vector_db=PgVector(
+                table_name="ia_website_documents",
+                db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+            ),
+            embedder=OllamaEmbedder(id=IA_MODEL, dimensions=3072),
+        )
+
+        combined_knowledge = CombinedKnowledgeBase(
+            sources=[
+                knowledge_base_web,
+                knowledge_base_documents,
+            ],
+            vector_db=PgVector(
+                table_name="ia_combined_documents",
+                db_url=IA_DB,
+                embedder=OllamaEmbedder(id=IA_MODEL, dimensions=3072),
+            ),
+        )
+
+        combined_knowledge.load(recreate=False)  # TODO: dar TTL o forzar
+        return combined_knowledge
